c93239dc714a624e5a855931344555889cca4402
tayyaba <taibabhutta@yahoo.com>
2024-08-20 13:31:26 -0400

    added-backend-logic-authentication

---
+++ src/users/user.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserSchema } from './user.schema';
+import { AuthService } from '../auth/auth.service';
+import { JwtModule } from '@nestjs/jwt'; // Import JwtModule
+import { config } from 'dotenv';
+config();
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET, 
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [UserController],
+  providers: [UserService, AuthService],
+  exports: [UserService],
+})
+export class UserModule {}
